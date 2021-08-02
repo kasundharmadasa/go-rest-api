1bc908d927520b75c1cfc812a4cb6f70441641b4
kasundharmadasa <kasunpdh@gmail.com>
2021-08-02 13:42:16 +0530

    update test packages

--- cmd/api/main_test.go
+++ cmd/api/main_test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"log"
 	"os"
 
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"sample.api.kasun.com/cmd/api/helpers"
@@ -45,9 +45,8 @@ func TestGetCustomers(t *testing.T) {
 	}
 
 	// Test the expected response body
-	expected := []byte(`{"customers":[{"ID":1,"Name":"Bob","Age":22,"Country":"MT",` +
-		`"Items":["Mouse","Keyboard"]}]}`)
-	if !bytes.Contains(reqRecorder.Body.Bytes(), expected) {
+	expected := `{"customers":[{"ID":1,"Name":"Bob","Age":22,"Country":"MT",`
+	if !strings.Contains(reqRecorder.Body.String(), expected) {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			reqRecorder.Body.String(), expected)
 	}
@@ -60,11 +59,11 @@ func TestGetCustomerById(t *testing.T) {
 
 	rp := &mock.CustomerModel{}
 
-	cs := DefaultCustomerService{
-		repository: rp,
-		logger:     logger,
-		helpers:    Helpers{},
-		errors:     Errors{},
+	cs := service.DefaultCustomerService{
+		Repository: rp,
+		Logger:     logger,
+		Helpers:    helpers.Helpers{},
+		Errors:     helpers.Errors{},
 	}
 	app := &application{
 		logger:          logger,
@@ -86,33 +85,10 @@ func TestGetCustomerById(t *testing.T) {
 	}
 
 	// Test the expected response body
-	expected := []byte(`{"customers":[{"ID":1,"Name":"Bob","Age":22,"Country":"MT",` +
-		`"Items":["Mouse","Keyboard"]}]}`)
-	if !bytes.Contains(reqRecorder.Body.Bytes(), expected) {
+	expected := `{"customers":[{"ID":1,"Name":"Bob","Age":22,"Country":"MT",`
+	if !strings.Contains(reqRecorder.Body.String(), expected) {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			reqRecorder.Body.String(), expected)
 	}
 
-}
-
-func TestCreateCustomer(t *testing.T) {
-
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	app := &application{
-		logger: logger,
-	}
-
-	req, err := http.NewRequest("POST", "/customers", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	reqRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(app.customerService.GetCustomers)
-	handler.ServeHTTP(reqRecorder, req)
-
-	// Test whether the response is OK
-	if status := reqRecorder.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
 }
